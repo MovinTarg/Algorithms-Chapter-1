3c7fed0e36f93742602aaea1454c30878e9054b3
MovinTarg <34400036+MovinTarg@users.noreply.github.com>
2018-03-29 12:23:49 -0400

    Add files via upload

--- Weekend Challenge/script.js
+++ Weekend Challenge/script.js
@@ -5,16 +5,45 @@ $(document).ready(function() {
         var questions = ['what is your favorite color?', 'do you like sports?', 'are your Hungry?']
         var answers = [];
         name = (window.prompt(nameQ));
-        if (name.length < 1) {
+        if (name === null) {
+            console.log("Maybe next time!")
+        } else if (name.length < 1) {
             for (var i = 0; i < questions.length; i++) {
                 answers.push(window.prompt('User ' + questions[i]));
+                if (answers[i] === null) {
+                    console.log("Stopping Quiz")
+                    if (answers.length > 1) {
+                        for (var j = 0; j < answers.length-1; j++) {
+                            console.log("You answered: " + answers[j]);
+                        }
+                    }
+                    break
+                }
+            }
+            if (answers.length > 2) {
+                for (var m = 0; m < answers.length; m++) {
+                    console.log("You answered: " + answers[m]);
+                }
             }
         } else {
-            for (var l = 0; l < questions.length; l++) {
-                answers.push(window.prompt(name + ' ' + questions[l]));
+            for (var k = 0; k < questions.length; k++) {
+                answers.push(window.prompt(name + ' ' + questions[k]));
+                if (answers[k] === null) {
+                    console.log("Stopping Quiz")
+                    if (answers.length > 1) {
+                        for (var l = 0; l < answers.length-1; l++) {
+                            console.log(name + " answered: " + answers[l]);
+                        }
+                    }
+                    break
+                }
+            }
+            if (answers.length > 2) {
+                for (var n = 0; n < answers.length; n++) {
+                    console.log(name + " answered: " + answers[n]);
+                }
             }
         }
-        console.log(answers);
     }
     questionaire();
 });
